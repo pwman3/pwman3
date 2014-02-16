3caf8acb789b9ee5b9772ed47d1c426c6805eaee
oz123 <nahumoz@gmail.com>
2014-02-16 18:49:36 +0100

    fix broken testing for config.py

--- pwman/tests/db_tests.py
+++ pwman/tests/db_tests.py
@@ -240,16 +240,20 @@ class ConfigTest(unittest.TestCase):
         self.tester.create()
 
     def test_config_write(self):
-        config.save(os.path.join(os.path.dirname(__file__), 'testing_config'))
-        self.assertTrue(os.path.exists('testing_config'))
-        os.remove('testing_config')
+        _filename = os.path.join(os.path.dirname(__file__),
+                                 'testing_config')
+        config._file = _filename
+        config.save(_filename)
+        self.assertTrue(_filename)
+        os.remove(_filename)
 
     def test_config_write_with_none(self):
-        config._file = os.path.join(os.path.dirname(__file__),
-                                    'testing_config')
+        _filename = os.path.join(os.path.dirname(__file__),
+                                 'testing_config')
+        config._file = _filename
         config.save()
-        self.assertTrue(os.path.exists('testing_config'))
-        os.remove('testing_config')
+        self.assertTrue(os.path.exists(_filename))
+        os.remove(_filename)
 
     def test_write_no_permission(self):
         # this test will pass if you run as root ...
