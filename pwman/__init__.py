4ac3f646784de42ba19571c25d67fb35865bef55
oz123 <nahumoz@gmail.com>
2014-02-25 12:23:55 +0100

    Release version 0.4.4

--- pwman/__init__.py
+++ pwman/__init__.py
@@ -20,7 +20,7 @@
 #============================================================================
 
 appname = "Pwman3"
-version = "0.4.4-dev"
+version = "0.4.4"
 website = "http://github.com/pwman3/pwman3"
 author = "Oz Nahum"
 authoremail = "nahumoz@gmail.com"
