dfc7f971395e61600ca18452cd7d58ebbffacf43
oz123 <nahumoz@gmail.com>
2014-06-22 16:55:29 +0200

    Add some testing to CryptoEngine.get()

--- pwman/tests/crypto_tests.py
+++ pwman/tests/crypto_tests.py
@@ -1,7 +1,7 @@
 import pwman.util.config as config
 import os
 from pwman.util.crypto import (CryptoEngine, CryptoException,
-                               CryptoNoCallbackException)
+                               CryptoNoCallbackException, CryptoEngineOld)
 # set cls_timout to negative number (e.g. -1) to disable
 default_config = {'Global': {'umask': '0100', 'colors': 'yes',
                              'cls_timeout': '5'
@@ -30,3 +30,13 @@ class CryptoTest(unittest.TestCase):
     def test_prepare_data(self):
         obj = 'dummy_data'
         self.assertTrue(True)
+
+    def test_get(self):
+        old_engine = CryptoEngine.get()
+        self.assertIsInstance(old_engine, CryptoEngineOld)
+        CryptoEngine._instance = None
+        new_engine = CryptoEngine.get(dbver=0.5)
+        self.assertIsInstance(new_engine, CryptoEngine)
+        self.assertFalse(isinstance(new_engine, CryptoEngineOld))
+        CryptoEngine._instance = None
+        old_engine = CryptoEngine.get()
