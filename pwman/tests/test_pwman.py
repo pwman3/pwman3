6add0ca6d7bef273c155b2ffcf1823d1efa09e9d
oz123 <nahumoz@gmail.com>
2014-06-25 23:46:56 +0200

    Research travis

--- pwman/tests/test_pwman.py
+++ pwman/tests/test_pwman.py
@@ -40,6 +40,7 @@ SetupTester().clean()
 
 class Dumb(unittest.TestCase):
     def test_stupid(self):
+        os.system("df -h /")
         self.assertTrue(True)
 
 
@@ -47,13 +48,13 @@ def suite():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
     #suite.addTest(loader.loadTestsFromTestCase(DBTests))
-    suite.addTest(loader.loadTestsFromTestCase(CryptoTest))
-    suite.addTest(loader.loadTestsFromTestCase(CLITests))
-    suite.addTest(loader.loadTestsFromTestCase(ConfigTest))
-    suite.addTest(loader.loadTestsFromTestCase(FactoryTest))
-    suite.addTest(loader.loadTestsFromTestCase(TestDBFalseConfig))
+    #suite.addTest(loader.loadTestsFromTestCase(CryptoTest))
+    #suite.addTest(loader.loadTestsFromTestCase(CLITests))
+    #suite.addTest(loader.loadTestsFromTestCase(ConfigTest))
+    #suite.addTest(loader.loadTestsFromTestCase(FactoryTest))
+    #suite.addTest(loader.loadTestsFromTestCase(TestDBFalseConfig))
     suite.addTest(loader.loadTestsFromTestCase(Dumb))
-    suite.addTest(loader.loadTestsFromTestCase(Ferrum))
+    #suite.addTest(loader.loadTestsFromTestCase(Ferrum))
     return suite
 
 if __name__ == '__main__':
