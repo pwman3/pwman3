5ce0088b689966dce0bc430ebb941b7f4d296c79
Oz N Tiram <oz.tiram@gmail.com>
2018-02-01 07:22:59 +0100

    Bump version to 0.9.7
    Signed-off-by: Oz N Tiram <oz.tiram@gmail.com>

--- setup.py
+++ setup.py
@@ -329,7 +329,7 @@ packages = find_packages(exclude=['tests', 'pwman/ui/templates'])
 
 
 setup(name='pwman3',
-      version='0.9.6',
+      version='0.9.7',
       description=("a command line password manager with support for multiple"
                    " databases."),
       long_description=long_description,
