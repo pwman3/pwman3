05016f583d8a9acf8bceee44b168462290b69068
Oz N Tiram <oz.tiram@gmail.com>
2018-08-23 07:31:27 +0200

    Bump version to 0.9.11

--- pwman/__init__.py
+++ pwman/__init__.py
@@ -41,7 +41,7 @@ appname = "pwman3"
 try:
     version = pkg_resources.get_distribution('pwman3').version
 except pkg_resources.DistributionNotFound:  # pragma: no cover
-    version = "0.9.10"
+    version = "0.9.11"
 
 
 class PkgMetadata(object):
