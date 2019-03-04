b633385c9d1ae7f0404d05080ef1db513ed3da4b
Oz N Tiram <oz.tiram@gmail.com>
2019-03-04 19:34:44 +0100

    Bump version to 0.10.0

--- setup.py
+++ setup.py
@@ -348,7 +348,7 @@ packages = find_packages(exclude=['tests', 'pwman/ui/templates'])
 
 
 setup(name='pwman3',
-      version='0.9.11',
+      version='0.10.0',
       description=("a command line password manager with support for multiple"
                    " databases."),
       long_description=long_description,
